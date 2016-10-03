8e30e8a115ccd20b06a6195fc171d6f769484daa
João C. Vieira <joaocv3@gmail.com>
2016-10-03 00:32:23 -0300

    chore: Add index fields

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,23 @@
 class WelcomeController < ApplicationController
   def index
+    @message = Message.new
+  end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.created_at = Time.now
+    
+    if @message.save
+      flash[:alert] = 'Message will be sent at #{params[:send_at]}'
+    else
+      flash[:alert] = 'Error'
+    end
+    redirect_to action: "index"  
+  end
+  
+  private
+  
+  def message_params
+    params.require(:message).permit(:content, :receiver_email, :receiver_name, :sender_name, :send_at)
   end
 end
