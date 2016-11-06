ebebd7d089aae26dd25da424e26d06503ed1a755
Joao C Vieira <joaocv3@gmail.com>
2016-11-06 16:54:17 -0200

    test: Update form fields names

--- features/create_message.feature
+++ features/create_message.feature
@@ -4,9 +4,8 @@ I want to create a new message
 
 Scenario: Create a new message to be sent later
 Given I am at the homepage 
-When I fill the receiver's e-mail "johndoe@gmail.com"
-And I fill the receiver's name "John Doe"
-And I fill the sender's name "Myself"
+When I fill the e-mail "johndoe@gmail.com"
+And I fill the name "John Doe"
 And I enter a date and time "27/09/2017" to send the message 
 And I fill the message content with "Have you been to Japan yet?" 
 And I send the form
--- features/step_definitions/create_message_steps.rb
+++ features/step_definitions/create_message_steps.rb
@@ -2,20 +2,16 @@ Given(/^I am at the homepage$/) do
   visit('/') 
 end
 
-When(/^I fill the receiver's e\-mail "([^"]*)"$/) do |arg1|
-  fill_in('message_receiver_email', :with => arg1) 
+When(/^I fill the e\-mail "([^"]*)"$/) do |arg1|
+  fill_in('message_email', :with => arg1) 
 end
 
-When(/^I fill the receiver's name "([^"]*)"$/) do |arg1|
-  fill_in('message_receiver_name', :with => arg1) 
-end
-
-When(/^I fill the sender's name "([^"]*)"$/) do |arg1|
-  fill_in('message_sender_name', :with => arg1) 
+When(/^I fill the name "([^"]*)"$/) do |arg1|
+  fill_in('message_name', :with => arg1) 
 end
 
 When(/^I enter a date and time "([^"]*)" to send the message$/) do |arg1|
-  fill_in('message_send_at', :with => arg1)
+  fill_in('message_send_on', :with => arg1)
 end
 
 When(/^I fill the message content with "([^"]*)"$/) do |arg1|
@@ -27,5 +23,5 @@ When(/^I send the form$/) do
 end
 
 Then(/^I should receive a confirmation message$/) do
-  find("div#msg", text: "Message will be sent on #{Message.last.send_at.to_s}")
+  find("div#msg", text: "Message will be sent on #{Message.last.send_on.to_s}")
 end
