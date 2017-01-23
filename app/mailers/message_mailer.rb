285b885b5cc4a55eaf2597e3c1f4e33d9654eacb
Joao C Vieira <joaocv3@gmail.com>
2017-01-23 01:05:41 -0200

    fix: Change attribute name inside mailer view

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,6 +2,6 @@ class MessageMailer < ApplicationMailer
   layout "mailer"
   def scheduled_message(message)
     @message = message
-    mail(to: message.receiver_email)
+    mail(to: message.email)
   end
 end
